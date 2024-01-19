78f909094b995fbf84109124b1329e8497a60def
Christopher Kemeza <dev.kemeza@gmail.com>
2024-01-19 15:46:03 -0500

    basic stage title scene

---
+++ src/app/sections/intro-scene.js
@@ -0,0 +1,11 @@
+
+
+const IntroScene = () => {
+    return (
+        <>
+            <p>Intro Scene</p>
+        </>
+    );
+}
+
+export default IntroScene;
---
+++ src/app/sections/title-scene.js
@@ -0,0 +1,75 @@
+import '../globals.css';
+import '../styles/scene-title.scss';
+
+const TitleScene = () => {
+  
+    return (
+      <section id="title" className="section-boundary">
+        <div className="title-grid">
+          <h1 className="title-word">
+            <span className="letter-wrap">
+              <span className="letter-character">S</span>
+            </span>
+            <span className="letter-wrap">
+              <span className="letter-character">i</span>
+            </span>
+            <span className="letter-wrap">
+              <span className="letter-character">n</span>
+            </span>
+            <span className="letter-wrap">
+              <span className="letter-character">c</span>
+            </span>
+            <span className="letter-wrap">
+              <span className="letter-character">e</span>
+            </span>
+          </h1>
+          <h1 className="title-word">
+            <span className="justify-words">
+              <span>
+                <span className="letter-wrap">
+                  <span className="letter-character">W</span>
+                </span>
+                <span className="letter-wrap">
+                  <span className="letter-character">e</span>
+                </span>
+              </span>
+              
+              <span>
+                <span className="letter-wrap">
+                  <span className="letter-character">L</span>
+                </span>
+                <span className="letter-wrap">
+                  <span className="letter-character">a</span>
+                </span>
+                <span className="letter-wrap">
+                  <span className="letter-character">s</span>
+                </span>
+                <span className="letter-wrap">
+                  <span className="letter-character">t</span>
+                </span>
+              </span>
+            </span>
+          </h1>
+          <h1 className="title-word">
+            <span className="letter-wrap">
+              <span className="letter-character">S</span>
+            </span>
+            <span className="letter-wrap">
+              <span className="letter-character">p</span>
+            </span>
+            <span className="letter-wrap">
+              <span className="letter-character">o</span>
+            </span>
+            <span className="letter-wrap">
+              <span className="letter-character">k</span>
+            </span>
+            <span className="letter-wrap">
+              <span className="letter-character">e</span>
+            </span>
+          </h1>
+        </div>
+      </section>
+    );
+  };
+  
+  export default TitleScene;
